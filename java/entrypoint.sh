65e08a5225c41fec72961952ab65b31e5e25490d
CSD <csd@minemelon.net>
2025-01-07 19:00:27 +0700

    .

--- java/entrypoint.sh
+++ java/entrypoint.sh
@@ -22,30 +22,15 @@
 # SOFTWARE.
 #
 
-# Default the TZ environment variable to UTC.
-TZ=${TZ:-UTC}
+TZ='Asia/Ho_Chi_Minh'
 export TZ
 
-# Set environment variable that holds the Internal Docker IP
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
-# Switch to the container's working directory
 cd /home/container || exit 1
 
-
-LOG_PREFIX="\033[1m\033[33mconsole@minemelon~ \033[0m"
-printf "${LOG_PREFIX} java -version\n"
-java -version
-
-
-# Convert all of the "{{VARIABLE}}" parts of the command into the expected shell
-# variable format of "${VARIABLE}" before evaluating the string and automatically
-# replacing the values.
 PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
 
-# Display the command we're running in the output, and then execute it with the env
-# from the container itself.
 printf "\033[1m\033[33mconsole@minemelon~ \033[0m%s\n" "$PARSED"
-# shellcheck disable=SC2086
 exec env ${PARSED}
